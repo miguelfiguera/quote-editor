19cbd1f39ca0ede06be8263e28bedb1ad06abdb9
Miguel Figuera <miguelqui725@gmail.com>
2023-04-13 13:40:10 -0400

    Add flash message
    This commit adds the logic for flash messages to:
    - layouts
    - controllers
    - helpers (creates a helper to show flash messages, so code stays dry)
    - views.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import RemovalController from "./removal_controller"
+application.register("removal", RemovalController)
